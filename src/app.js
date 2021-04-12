b623061fadd89ad87292269a1f8c4629bfc1ec40
Erwin Gulfo Vega <erwin.gulfo@upb.edu.co>
2021-04-12 12:21:45 -0500

    Se realizan cambios en el index.js del backend

--- src/app.js
+++ src/app.js
@@ -11,6 +11,6 @@ app.use('/api/users', require('./routes/users'))
 app.use('/api/notes', require('./routes/notes'))
 app.use('/api/recursos', require('./routes/recursos'))
 app.use('/api/tipos', require('./routes/tipos'))
-app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(express.static(path.join('../client/build')));
 
 module.exports= app; 
