f855bbe1295a83587378cf32d2c20d3adf1aeb78
Erwin Gulfo Vega <erwin.gulfo@upb.edu.co>
2021-04-12 12:05:47 -0500

    cambios en el index.js del backend

--- src/index.js
+++ src/index.js
@@ -3,9 +3,14 @@ require('dotenv').config();
 const app= require('./app');
 
 require('./database');
+const path = require('path');
 
 function main(){
 	
+	app.get('*', (req, res) => {
+		res.sendFile(path.join(__dirname+'/client/build/index.html'));
+	  });
+
 	app.listen(process.env.PORT, ()=>{
 		console.log("App corriendo por el puerto:", process.env.PORT);
 	});
